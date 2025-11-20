3be1d14168239eb8359cf054981175f22970d653
JuniorThap <Thapanawatsuriyaroj@gmail.com>
2025-11-20 19:48:40 +0700

    commit again good good

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     font = QFont("Segoe UI", 10)
     app.setFont(font)
     
-    photobooth = PhotoBoothApp(camera_index=0, server_url="https://82df8cf1ab68.ngrok-free.app")
+    photobooth = PhotoBoothApp(camera_index=2, server_url="https://82df8cf1ab68.ngrok-free.app")
     photobooth.show()
     
     # Cleanup on exit
